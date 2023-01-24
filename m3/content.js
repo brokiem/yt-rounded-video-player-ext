965894a6da7c081f68fd953c4580116b1a44e8bb
brokiem <milyhqvideo@gmail.com>
2023-01-25 04:24:13 +0800

    Use MutationObserver to change the border radius

--- m3/content.js
+++ m3/content.js
@@ -2,9 +2,24 @@ if (typeof browser === "undefined") {
     var browser = chrome;
 }
 
-browser.storage.sync.get("ytrvp-border-radius", function(items) {
+browser.storage.sync.get("ytrvp-border-radius", function (items) {
     if (!browser.runtime.error) {
-        const video = document.getElementsByClassName('html5-video-player')[0];
-        video.style.borderRadius = items['ytrvp-border-radius'] ?? '12px';
+        let observer = new MutationObserver(mutations => {
+            for (let mutation of mutations) {
+                for (let node of mutation.addedNodes) {
+                    if (node == null || node.tagName == null) {
+                        continue;
+                    }
+                    if (node.tagName.toLowerCase() !== 'video') {
+                        continue;
+                    }
+
+                    const video = document.getElementsByClassName('html5-video-player')[0];
+                    video.style.borderRadius = items['ytrvp-border-radius'] ?? '12px';
+                }
+            }
+        });
+
+        observer.observe(document, { childList: true, subtree: true });
     }
 });
